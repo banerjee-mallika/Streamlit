0d6b0261edc90df85a406666ddf77f7d9c45463f
banerjee-mallika <mallika.chatterjee08@gmail.com>
2024-06-22 00:02:04 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,6 +31,17 @@ st.line_chart(sales_by_month, y="Sales")
 
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
+import streamlit as st
+
+# List of categories
+categories = ['Furniture', 'Office Supplies', 'Technology']
+
+# Add a dropdown selectbox to select the category
+selected_category = st.selectbox('Select Category', categories)
+
+# Display the selected category
+st.write('You selected:', selected_category)
+
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 st.write("### (3) show a line chart of sales for the selected items in (2)")
 st.write("### (4) show three metrics (https://docs.streamlit.io/library/api-reference/data/st.metric) for the selected items in (2): total sales, total profit, and overall profit margin (%)")
