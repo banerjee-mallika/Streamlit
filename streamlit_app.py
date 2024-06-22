736f70e49c44c4c05e31e247c23a379bd82c4b88
banerjee-mallika <mallika.chatterjee08@gmail.com>
2024-06-22 00:54:35 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -45,9 +45,8 @@ st.write('You selected:', selected_category)
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 import streamlit as st
 
-options = st.multiselect(
-    "sub_categories",
-    ['Bookcases', 'Chairs', 'Furnishings', 'Tables'], ['Appliances', 'Art', 'Binders', 'Envelopes', 'Fasteners', 'Labels', 'Storage', 'Supplies'], ['Accessories', 'Copiers', 'Machines', 'Paper','Phones']
+options = st.multiselect("sub_categories",
+    ['Bookcases', 'Chairs', 'Furnishings', 'Tables'], ['Appliances', 'Art', 'Binders', 'Envelopes', 'Fasteners', 'Labels', 'Storage', 'Supplies'], ['Accessories', 'Copiers', 'Machines', 'Paper','Phones'])
 
 st.write("You selected:", options)
 
