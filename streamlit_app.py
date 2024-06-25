ec68e0a81b7d724cf5286958346b5150d4a7f5e8
banerjee-mallika <mallika.chatterjee08@gmail.com>
2024-06-25 01:49:33 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,16 +31,7 @@ st.line_chart(sales_by_month, y="Sales")
 
 st.write("## Your additions")
 st.write("### (1) add a drop down for Category (https://docs.streamlit.io/library/api-reference/widgets/st.selectbox)")
-import streamlit as st
-
-# List of categories
-categories = ['Furniture', 'Office Supplies', 'Technology']
-
-# Add a dropdown selectbox to select the category
-selected_category = st.selectbox('Select Category', categories)
-
-# Display the selected category
-st.write('You selected:', selected_category)
+category = st.selectbox("Select a Category", df['Category'].unique())
 
 st.write("### (2) add a multi-select for Sub_Category *in the selected Category (1)* (https://docs.streamlit.io/library/api-reference/widgets/st.multiselect)")
 categories = ['Furniture', 'Office Supplies', 'Technology']
